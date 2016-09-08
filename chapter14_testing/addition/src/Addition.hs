efd093e2e2104434798845124e6dcc617bf0de43
Emre Sevinç <emre.sevinc@gmail.com>
2016-09-08 13:45:35 +0200

    Add simple HSpec and QuickCheck example project

---
+++ chapter14_testing/addition/src/Addition.hs
@@ -0,0 +1,70 @@
+-- Addition.hs
+module Addition where
+
+import Test.Hspec
+import Test.QuickCheck
+
+genBool :: Gen Bool
+genBool = choose (False, True)
+
+genBool' :: Gen Bool
+genBool' = elements [False, True]
+
+genOrdering :: Gen Ordering
+genOrdering = elements [LT, EQ, GT]
+
+genChar :: Gen Char
+genChar = elements ['a'..'z']
+
+genTuple :: (Arbitrary a, Arbitrary b) => Gen (a, b)
+genTuple = do
+  a <- arbitrary
+  b <- arbitrary
+  return (a, b)
+
+genThreeple :: (Arbitrary a, Arbitrary b, Arbitrary c) => Gen (a, b, c)
+genThreeple = do
+  a <- arbitrary
+  b <- arbitrary
+  c <- arbitrary
+  return (a, b, c)
+
+genEither :: (Arbitrary a, Arbitrary b) => Gen (Either a b)
+genEither = do
+  a <- arbitrary
+  b <- arbitrary
+  elements [Left a, Right b]
+
+-- equal probability
+genMaybe :: Arbitrary a => Gen (Maybe a)
+genMaybe = do
+  a <- arbitrary
+  elements [Nothing, Just a]
+
+-- What QuickCheck actually does
+-- so you get more Just values
+genMaybe' :: Arbitrary a => Gen (Maybe a)
+genMaybe' = do
+  a <- arbitrary
+  frequency [ (1, return Nothing)
+            , (3, return (Just a))]
+-- frequency :: [(Int, Gen a)] -> Gen a
+
+prop_additionGreater :: Int -> Bool
+-- prop_additionGreater x = x + 1 > x
+prop_additionGreater x = x + 0 > x
+
+runQc :: IO ()
+runQc = quickCheck prop_additionGreater
+
+main :: IO ()
+main = hspec $ do
+  describe "Addition" $ do
+    it "1 + 1 is greater than 1" $ do
+      (1 + 1) > 1 `shouldBe` True
+    it "2 + 2 is equal to 4" $ do
+      2 + 2 `shouldBe` 4
+    it "x + 1 is always greater than x" $ do
+      property $ \x -> x + 1 > (x :: Int)
+
+
