665aa35a4050b592db3591b5ee4054192f92a2cf
Emre Sevinç <emre.sevinc@gmail.com>
2016-09-09 10:25:48 +0200

    Add solution for algebra use in optional monoid

---
+++ chapter15_monoid_semigroup/ex.hs
@@ -0,0 +1,15 @@
+import Data.Monoid
+
+data Optional a =
+   Nada
+ | Only a
+ deriving (Eq, Show)
+
+instance Monoid a => Monoid (Optional a) where
+  mempty = Nada
+  mappend Nada x = x
+  mappend x Nada = x
+  mappend (Only x) (Only y) = Only (x <> y)
+
+
+
