bb8b869df69f96078e55de09f5f28eabcc74d504
Emre Sevinç <emre.sevinc@gmail.com>
2016-09-12 08:22:28 +0200

    (WIP) Add simplest semigroup and QuickCheck solutions

--- chapter15_monoid_semigroup/semigroupExercise/app/Main.hs
+++ chapter15_monoid_semigroup/semigroupExercise/app/Main.hs
@@ -1,8 +1,113 @@
+-- Given a datatype, implement the Semigroup instance. Add Semi-group constraints
+-- to type variables where needed. Use the Semigroup class from the semigroups
+-- library or write your own. When we use <>, we mean the infix mappend from the
+-- Semigroup typeclass.
+--
+-- Note We’re not always going to derive every instance you may want or need in
+-- the datatypes we provide for exercises. We expect you to know what you need
+-- and to take care of it yourself by this point.
+
+-- 1. Validate all of your instances with QuickCheck. Since Semigroup’s
+-- only law is associativity, that’s the only property you need to reuse.
+
 module Main where
 
 import Lib
 
 import Data.Semigroup
+import Test.QuickCheck
+import Test.QuickCheck.Gen (oneof)
+
+data Trivial = Trivial deriving (Eq, Show)
+
+instance Semigroup Trivial where
+  _ <> _ = Trivial
+
+instance Arbitrary Trivial where
+ arbitrary = return Trivial
+
+semigroupAssoc :: (Eq m, Semigroup m) => m -> m -> m -> Bool
+semigroupAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+type TrivialAssoc = Trivial -> Trivial -> Trivial -> Bool
+
+newtype Identity a = Identity a
+
+checkTrivial :: IO ()
+checkTrivial = quickCheck (semigroupAssoc :: TrivialAssoc)
+
+newtype BoolConj = BoolConj Bool deriving (Eq, Show)
+
+instance Semigroup BoolConj where
+  BoolConj True  <> BoolConj False = BoolConj False
+  BoolConj True  <> BoolConj True  = BoolConj True
+  BoolConj False <> BoolConj True  = BoolConj False
+  BoolConj False <> BoolConj False  = BoolConj False
+
+boolConjGen :: Gen (BoolConj)
+boolConjGen = do
+  a <- arbitrary
+  return (BoolConj a)
+  
+instance Arbitrary BoolConj where
+  arbitrary = boolConjGen
+
+checkBoolConj :: IO ()
+checkBoolConj = quickCheck (semigroupAssoc :: BoolConj -> BoolConj -> BoolConj -> Bool)
+
+
+data Three a b c = Three a b c
+
+instance (Eq a, Eq b, Eq c) => Eq (Three a b c) where
+  (Three x y z) == (Three x' y' z') = x == x' && y == y' && z == z'
+
+instance Semigroup (Three a b c) where
+  (Three x y z) <> (Three xx yy zz) = (Three x y z)
+
+threeGen :: (Arbitrary a, Arbitrary b, Arbitrary c) => Gen (Three a b c)
+threeGen = do
+  x <- arbitrary
+  y <- arbitrary
+  z <- arbitrary
+  return (Three x y z)
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) =>  Arbitrary (Three a b c) where
+  arbitrary = threeGen
+
+threeGenInt :: Gen (Three Int Int Int)
+threeGenInt = threeGen
+
+-- checkThree :: IO ()
+-- checkThree = quickCheck (semigroupAssoc :: (Three a b c) -> (Three a b c) -> (Three a b c) -> Bool)
+
+data Or a b =
+    Fst a
+  | Snd b
+  deriving (Eq, Show)
+
+instance Semigroup (Or a b) where
+  Fst a <> Snd b = Snd b
+  Fst a <> Fst b = Fst b
+  Snd a <> Fst b = Snd a
+  Snd a <> Snd b = Snd a
+
+orGenEqual :: (Arbitrary a, Arbitrary b) => Gen (Or a b)
+orGenEqual = do
+  a <- arbitrary
+  b <- arbitrary
+  oneof [return $ Fst a,
+         return $ Snd b]
+
+orGenIntInt :: Gen (Or Int Int)
+orGenIntInt = orGenEqual
+
+checkOr :: IO ()
+checkOr = quickCheck (semigroupAssoc :: (Or a b) -> (Or a b) -> (Or a b) -> Bool)
+
 
 main :: IO ()
-main = someFunc
+main = do
+  checkTrivial
+  checkBoolConj
+  
+
