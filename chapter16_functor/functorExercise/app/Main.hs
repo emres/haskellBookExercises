2e2d432e33ce31f52e9656c27be7891025489732
Emre Sevinç <emre.sevinc@gmail.com>
2016-09-14 16:42:28 +0200

    (WIP) Add Functor instance for Identity a

--- chapter16_functor/functorExercise/app/Main.hs
+++ chapter16_functor/functorExercise/app/Main.hs
@@ -2,6 +2,11 @@ module Main where
 
 import Lib
 
+newtype Identity a = Identity a
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
 main :: IO ()
 main = 
   putStrLn "Let's solve some Functor exercises!"
