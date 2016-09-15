f856f76ba0e2e87c0040e8fcee1b3a3ca669769c
Emre Sevinç <emre.sevinc@gmail.com>
2016-09-15 15:06:40 +0200

    Add quickCheck for checking identity law for Identity a

--- chapter16_functor/functorExercise/app/Main.hs
+++ chapter16_functor/functorExercise/app/Main.hs
@@ -1,12 +1,38 @@
 module Main where
 
 import Lib
+import Test.QuickCheck
 
+--------------------------------------------------------------------------------
+functorIdentity :: (Functor f, Eq (f a)) => f a -> Bool
+functorIdentity f =
+  fmap id f == f
+
+functorCompose :: (Eq (f c), Functor f) => (a -> b) -> (b -> c) -> f a -> Bool
+functorCompose f g x =
+  (fmap g (fmap f x)) == (fmap (g . f) x)
+
+
+--------------------------------------------------------------------------------
 newtype Identity a = Identity a
+  deriving (Eq, Show)
 
 instance Functor Identity where
   fmap f (Identity a) = Identity (f a)
 
+identityGen :: (Arbitrary a) => Gen (Identity a)
+identityGen = do
+  x <- arbitrary
+  return (Identity x)
+
+instance (Arbitrary a) => Arbitrary (Identity a) where
+  arbitrary = identityGen
+
+checkIdentityIdentity :: IO ()
+checkIdentityIdentity = quickCheck $ \x -> functorIdentity (x :: (Identity Int))
+--------------------------------------------------------------------------------
+
 main :: IO ()
-main = 
+main = do
   putStrLn "Let's solve some Functor exercises!"
+  checkIdentityIdentity
