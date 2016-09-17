024c298ec28f7d568d514f9200e7ed2f7b87c290
Emre Sevinç <emre.sevinc@gmail.com>
2016-09-17 13:48:21 +0200

    Rearrange More a b, so that Functor works

--- chapter16_functor/functorExercise/app/Main.hs
+++ chapter16_functor/functorExercise/app/Main.hs
@@ -191,8 +191,7 @@ instance Functor (Sum e) where
   fmap f (Second b) = Second b
 
 
---------
-
+----------
 data Company a c b =
     DeepBlue a c
   | Something b
@@ -202,6 +201,15 @@ instance Functor (Company e e') where
   fmap _ (DeepBlue a c) = DeepBlue a c
 
 
+----------
+data More b a =
+    L a b a
+  | R b a b
+  deriving (Eq, Show)
+
+instance Functor (More x) where
+  fmap f (L a b a') = L (f a) b (f a')
+  fmap f (R b a b') = R b (f a) b'
 
 main :: IO ()
 main = do
