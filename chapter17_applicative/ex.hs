dca01d18fb58857ca5bb1f60b0f129a50b5e0e68
Emre Sevinç <emre.sevinc@gmail.com>
2016-09-20 15:40:51 +0200

    Add some more initial Applicative exercise solutions

--- chapter17_applicative/ex.hs
+++ chapter17_applicative/ex.hs
@@ -33,8 +33,22 @@ max' :: Int -> Int -> Int
 max' = max
 
 maxed :: Maybe Int
-maxed = max' <$> x2  <*> y2
+maxed = max' <$> x2 <*> y2
 
+----------
+xs = [1, 2, 3]
+ys = [4, 5, 6]
+
+x3 :: Maybe Integer
+x3 = lookup 3 $ zip xs ys
+
+y3 :: Maybe Integer
+y3 = lookup 2 $ zip xs ys
+
+summed :: Maybe Integer
+summed = (fmap . fmap) sum (,) <$> x3 <*> y3
+
+--------------------------------------------------------------------------------
 main :: IO ()
 main = do
   putStrLn "Let's do some Applicative exercises!"
