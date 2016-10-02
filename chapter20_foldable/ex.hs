75906f02d2641b406a802133cf3d3e07dea24ee4
Emre Sevinç <emre.sevinc@gmail.com>
2016-10-02 15:17:25 +0200

    Implement minimum using foldr from Foldable

--- chapter20_foldable/ex.hs
+++ chapter20_foldable/ex.hs
@@ -14,5 +14,22 @@ product = foldr (*) 1
 elem :: (Foldable t, Eq a) => a -> t a -> Bool
 elem = any . (==)
 
---minimum :: (Foldable t, Ord a) => t a -> Maybe a
---minimum l = foldr (\x y -> if x < y then x else y) 0 l
+--------------------------------------------------------------------------------
+-- For details, alternatives, and discussions, see the following:
+-- http://stackoverflow.com/q/37398457/236007
+-- http://stackoverflow.com/q/12216886/236007
+minimum :: (Foldable t, Ord a) => t a -> Maybe a
+minimum = foldr maybeMin Nothing
+  where
+    maybeMin x Nothing = Just x
+    maybeMin x (Just y) = Just (min x y)
+
+-- Using type holes
+-- mini :: (Foldable t, Ord a) => t a -> Maybe a
+-- mini xs = foldr _ Nothing xs
+
+
+
+
+main :: IO ()
+main = putStrLn "Let's do some Foldable exercises!"
