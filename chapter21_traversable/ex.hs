0be4855b007c391593743159f4e1b845daa5a10f
Emre Sevinç <emre.sevinc@gmail.com>
2016-10-05 14:58:20 +0200

    Implement a Traversable instance for Constant a b

--- chapter21_traversable/ex.hs
+++ chapter21_traversable/ex.hs
@@ -18,6 +18,19 @@ instance Traversable Identity where
   --traverse f (Identity x) = Identity <$> f x
   traverse f (Identity x) = fmap Identity (f x)
 
+--------------------------------------------------------------------------------
+newtype Constant a b =
+  Constant { getConstant :: a }
 
+instance Functor (Constant a) where
+  fmap _ (Constant a) = Constant a
+
+instance Foldable (Constant a) where
+  foldr _ z (Constant _) = z
+
+instance Traversable (Constant a) where
+  traverse f (Constant x) = pure $ Constant x
+
+--------------------------------------------------------------------------------
 main :: IO ()
 main = putStrLn "Let's do some Traversable exercises!"
