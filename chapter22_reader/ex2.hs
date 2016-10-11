ba9102194a67ef047bd4c239f0f784edf323a82d
Emre Sevinç <emre.sevinc@gmail.com>
2016-10-11 13:19:23 +0200

    Combine two functions in various ways

--- chapter22_reader/ex2.hs
+++ chapter22_reader/ex2.hs
@@ -1,4 +1,5 @@
 import Data.Char
+import Control.Applicative
 
 cap :: [Char] -> [Char]
 cap xs = map toUpper xs
@@ -6,5 +7,17 @@ cap xs = map toUpper xs
 rev :: [Char] -> [Char]
 rev xs = reverse xs
 
-main :: IO ()
-main = putStrLn "Let's do some Reader exercises!"
+composed :: [Char] -> [Char]
+composed = cap . rev
+
+fmapped :: [Char] -> [Char]
+fmapped = cap <$> rev
+
+tupled :: [Char] -> ([Char], [Char])
+tupled = liftA2 (,) cap rev
+
+tupled' :: [Char] -> ([Char], [Char])
+tupled' = do
+  x <- cap
+  y <- rev
+  return (x, y)
