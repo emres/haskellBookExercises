39716c8005dce6e94d46ecd78e0599e9b4364c45
Emre Sevinç <emre.sevinc@gmail.com>
2016-10-12 12:47:16 +0200

    Implement `asks :: (r -> a) -> Reader r a`

--- chapter22_reader/ex2.hs
+++ chapter22_reader/ex2.hs
@@ -28,6 +28,8 @@ newtype Reader r a =
 ask :: Reader a a
 ask = Reader id
 
-
 myLiftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
 myLiftA2 f x y = f <$> x <*> y
+
+asks :: (r -> a) -> Reader r a
+asks f = Reader f
