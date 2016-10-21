4aed73387f3c3bc435ae1cbd9b3c32491aaa158b
Emre Sevinç <emre.sevinc@gmail.com>
2016-10-21 12:03:29 +0200

    Finish Chapter 22 exercises

--- chapter22_reader/ReaderPractice.hs
+++ chapter22_reader/ReaderPractice.hs
@@ -59,3 +59,43 @@ x2 = liftA2 (,) ys zs
 
 x3 :: Integer -> (Maybe Integer, Maybe Integer)
 x3 n = (,) (z' n) (z' n)
+
+uncurry' :: (a -> b -> c) -> (a, b) -> c
+uncurry' f t =
+  case t of
+    (arg1, arg2) -> f arg1 arg2
+    
+-- that first argument is a function
+-- in this case, we want it to be addition
+-- summed is just uncurry with addition as
+-- the first argument
+
+summed :: Num c => (c, c) -> c
+summed = uncurry' (+)
+
+-- And now we’ll make a function similar to some we’ve seen before
+-- that lifts a boolean function over two partially-applied functions:
+bolt :: Integer -> Bool
+
+-- use &&, >3, <8
+bolt = (&&) <$> (>3) <*> (<8)
+
+sequA :: Integral a => a -> [Bool]
+sequA m = sequenceA [(>3), (<8), even] m
+
+s' :: Maybe Integer
+s' = summed <$> ((,) <$> xs <*> ys)
+
+main :: IO ()
+main = do
+  print $ sequenceA [Just 3, Just 2, Just 1]
+  print $ sequenceA [x, y]
+  print $ sequenceA [xs, ys]
+  print $ summed <$> ((,) <$> xs <*> ys)
+  print $ fmap summed ((,) <$> xs <*> zs)
+  print $ bolt 7
+  print $ fmap bolt z
+  print $ sequenceA [(>3), (<8), even] 7
+  print $ foldr (&&) True $ sequA 7
+  print $ sequA $ fromMaybe 0 s'
+  print $ bolt $ fromMaybe 0 ys
