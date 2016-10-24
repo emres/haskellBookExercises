137d246fbb63cae85e7410572203056e470d0d98
Emre Sevinç <emre.sevinc@gmail.com>
2016-10-24 10:20:20 +0200

    Refactored rollsToGetTwenty to take limit as argument

---
+++ chapter23_state/RandomExample2.hs
@@ -0,0 +1,78 @@
+module RandomExample2 where
+
+import Control.Applicative (liftA3)
+import Control.Monad (replicateM)
+import Control.Monad.Trans.State
+import System.Random
+
+data Die =
+    DieOne
+  | DieTwo
+  | DieThree
+  | DieFour
+  | DieFive
+  | DieSix
+  deriving (Eq, Show)
+
+intToDie :: Int -> Die
+intToDie n =
+  case n of
+    1 -> DieOne
+    2 -> DieTwo
+    3 -> DieThree
+    4 -> DieFour
+    5 -> DieFive
+    6 -> DieSix
+    -- Use this tactic _extremely_ sparingly.
+    x -> error $ "intToDie got non 1-6 integer: " ++ show x
+
+rollDie :: State StdGen Die
+rollDie = state $ do
+  (n, s) <- randomR (1, 6)
+  return (intToDie n, s)
+
+rollDie' :: State StdGen Die
+rollDie' =
+  intToDie <$> state (randomR (1, 6))
+
+rollDieThreeTimes' :: State StdGen (Die, Die, Die)
+rollDieThreeTimes' =
+  liftA3 (,,) rollDie rollDie rollDie
+
+infiniteDie :: State StdGen [Die]
+infiniteDie = repeat <$> rollDie
+
+-- take 6 $ evalState infiniteDie (mkStdGen 0)
+
+-- replicateM :: Monad m => Int -> m a -> m [a]
+nDie :: Int -> State StdGen [Die]
+nDie n = replicateM n rollDie
+
+-- evalState (nDie 5) (mkStdGen 0)
+
+rollsToGetTwenty :: StdGen -> Int
+rollsToGetTwenty g = go 0 0 g
+  where
+    go :: Int -> Int -> StdGen -> Int
+    go sum count gen
+      | sum >= 20 = count
+      | otherwise =
+          let (die, nextGen) = randomR (1, 6) gen
+          in go (sum + die) (count + 1) nextGen
+
+-- rollsToGetTwenty (mkStdGen 0)
+
+-- randomIO :: Random a => IO a
+
+-- (rollsToGetTwenty . mkStdGen) <$> randomIO
+
+-- Refactor rollsToGetTwenty into having the limit be a function argument.
+rollsToGetN :: Int -> StdGen -> Int
+rollsToGetN n g = go 0 0 g
+  where
+    go :: Int -> Int -> StdGen -> Int
+    go sum count gen
+      | sum >= n = count
+      | otherwise =
+          let (die, nextGen) = randomR (1, 6) gen
+          in go (sum + die) (count + 1) nextGen
