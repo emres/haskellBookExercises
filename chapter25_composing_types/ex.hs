6433279bb722e75fe13291338046b65974276645
Emre Sevinç <emre.sevinc@gmail.com>
2016-10-26 11:04:59 +0200

    Implement Twinplicative: Applicative for Compose

--- chapter25_composing_types/ex.hs
+++ chapter25_composing_types/ex.hs
@@ -13,9 +13,21 @@ instance Functor Identity where
 instance (Functor f, Functor g) => Functor (Compose f g) where
   fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
 
+-- For more details, see the following:
+-- https://carlo-hamalainen.net/blog/2014/1/2/applicatives-compose-monads-do-not
+-- https://www.youtube.com/watch?v=AjtQ0sQaHn0
+-- http://stackoverflow.com/questions/7040844/applicatives-compose-monads-dont
+-- http://stackoverflow.com/questions/29453915/composing-monads-v-applicative-functors
+
 instance (Applicative f, Applicative g) => Applicative (Compose f g) where
   pure :: a -> Compose f g a
-  pure = undefined
+  pure a = Compose (pure (pure a))
 
-  (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
-  (Compose f) <*> (Compose a) = undefined
+  (<*>) :: Compose f g (a -> b)
+        -> Compose f g a
+        -> Compose f g b
+  (Compose f) <*> (Compose a) = Compose ((<*>) <$> f <*> a)
+                                -- Or: Compose (pure (<*>) <*> f <*> a)
+
+--------------------------------------------------------------------------------
+
