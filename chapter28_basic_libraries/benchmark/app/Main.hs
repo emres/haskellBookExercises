09b6b7d48ac2fc989f4672cee8c61621015c7948
Emre Sevinç <emre.sevinc@gmail.com>
2016-11-08 09:25:24 +0100

    Implement append and add benchmarking code for DList

--- chapter28_basic_libraries/benchmark/app/Main.hs
+++ chapter28_basic_libraries/benchmark/app/Main.hs
@@ -31,9 +31,24 @@ snoc wrappedFnForXs x = DL $ (unDL wrappedFnForXs . (x:))
 
 -- Append dlists.
 append :: DList a -> DList a -> DList a
-append = undefined
+append wrappedFnForXs wrappedFnForYs = DL $
+                                       (unDL wrappedFnForXs) .
+                                       (unDL wrappedFnForYs)
 {-# INLINE append #-}
 
+schlemiel :: Int -> [Int]
+schlemiel i = go i []
+  where go 0 xs = xs
+        go n xs = go (n-1) ([n] ++ xs)
+
+constructDlist :: Int -> [Int]
+constructDlist i = toList $ go i empty
+  where go 0 xs = xs
+        go n xs = go (n-1) (singleton n `append` xs)
+
 main :: IO ()
-main = do
-  putStrLn "some benchmarking for DList"
+main = defaultMain
+  [ bench "concat list" $ whnf schlemiel 123456
+  , bench "concat dlist" $ whnf constructDlist 123456
+  ]
+
