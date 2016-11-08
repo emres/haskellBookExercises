4c107f528eedd140b6f5d30407ccfa65aa6d0237
Emre Sevinç <emre.sevinc@gmail.com>
2016-11-08 10:04:59 +0100

    Use the `>>` ("and then") to combine

--- chapter28_basic_libraries/benchmark/app/Main.hs
+++ chapter28_basic_libraries/benchmark/app/Main.hs
@@ -47,8 +47,8 @@ constructDlist i = toList $ go i empty
         go n xs = go (n-1) (singleton n `append` xs)
 
 main :: IO ()
-main = do
-  putStrLn "Let's run some DList benchmarks!"
+main = 
+  putStrLn "Let's run some DList benchmarks!" >>
   defaultMain
     [ bench "concat list" $ whnf schlemiel 123456
     , bench "concat dlist" $ whnf constructDlist 123456
