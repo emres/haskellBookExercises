7a898dc06d426e1491ceee129888db0869296428
Emre Sevinç <emre.sevinc@gmail.com>
2016-11-23 10:21:18 +0100

    Add SQLite database creation code

--- chapter31_final_project/fingerd/src/Main.hs
+++ chapter31_final_project/fingerd/src/Main.hs
@@ -20,6 +20,78 @@ import qualified Data.ByteString as BS
 import Network.Socket.ByteString (recv, sendAll)
 import Text.RawString.QQ
 
+
+data User =
+  User {
+      userId :: Integer
+    , username :: Text
+    , shell :: Text
+    , homeDirectory :: Text , realName :: Text
+    , phone :: Text
+  } deriving (Eq, Show)
+
+
+instance FromRow User where
+  fromRow = User <$> field
+                 <*> field
+                 <*> field
+                 <*> field
+                 <*> field
+                 <*> field
+
+instance ToRow User where
+  toRow (User id_ username shell homeDir realName phone) =
+    toRow (id_, username, shell, homeDir, realName, phone)
+
+createUsers :: Query
+createUsers = [r|
+CREATE TABLE IF NOT EXISTS users
+  (id INTEGER PRIMARY KEY AUTOINCREMENT,
+   username TEXT UNIQUE,
+   shell TEXT,
+   homeDirectory TEXT,
+   realName TEXT,
+   phone TEXT)
+|]
+
+
+insertUser :: Query
+insertUser = "INSERT INTO users VALUES (?, ?, ?, ?, ?, ?)"
+
+allUsers :: Query
+allUsers = "SELECT * FROM users"
+
+getUserQuery :: Query
+getUserQuery = "SELECT * from users where username = ?"
+
+data DuplicateData =
+  DuplicateData
+  deriving (Eq, Show, Typeable)
+
+instance Exception DuplicateData
+
+
+type UserRow = (Null, Text, Text, Text, Text, Text)
+
+getUser :: Connection -> Text -> IO (Maybe User)
+getUser conn username = do
+  results <- query conn getUserQuery (Only username)
+  case results of
+    [] -> return $ Nothing
+    [user] -> return $ Just user
+    _ -> throwIO DuplicateData
+
+createDatabase :: IO ()
+createDatabase = do
+  conn <- open "finger.db"
+  execute_ conn createUsers
+  execute conn insertUser meRow
+  rows <- query_ conn allUsers
+  mapM_ print (rows :: [User])
+  SQLite.close conn
+  where meRow :: UserRow
+        meRow = (Null, "callen", "/bin/zsh","/home/callen", "Chris Allen", "555-123-4567")
+
 main :: IO ()
 main = do
   putStr "let's have some action."
